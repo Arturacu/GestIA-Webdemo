d070ecfdd1fe0c165543293ed238a58cdd30d384
Arturacu <arturoacuaviva@gmail.com>
2020-09-13 19:52:24 +0200

    Updates

---
+++ precache-manifest.fd42d95e36e5d26de00f394e559db438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b837aadeef2c887c4f7be07683345a7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b3395352b1b045bed42",
+    "url": "/static/css/main.65a6af82.chunk.css"
+  },
+  {
+    "revision": "4f4660176e2e6f4dacdb",
+    "url": "/static/js/2.a23894df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a23894df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3395352b1b045bed42",
+    "url": "/static/js/main.8a6c39bd.chunk.js"
+  },
+  {
+    "revision": "e64c7bc46e47c6113f2a",
+    "url": "/static/js/runtime-main.52dc3122.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GestIA-Webdemo/precache-manifest.f6b249e1573b0d0b55beac13d1fdadc3.js"
+  "/precache-manifest.fd42d95e36e5d26de00f394e559db438.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GestIA-Webdemo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
