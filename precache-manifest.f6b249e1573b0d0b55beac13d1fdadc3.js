f91302183d77a62a037191576d52a3b8da2a1dc2
Arturacu <arturoacuaviva@gmail.com>
2020-09-13 19:53:33 +0200

    Updates

---
+++ precache-manifest.f6b249e1573b0d0b55beac13d1fdadc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee74f71894e085bffb930ac53abace9",
+    "url": "/GestIA-Webdemo/index.html"
+  },
+  {
+    "revision": "746a62aee4b5a5acb68c",
+    "url": "/GestIA-Webdemo/static/css/main.65a6af82.chunk.css"
+  },
+  {
+    "revision": "4f4660176e2e6f4dacdb",
+    "url": "/GestIA-Webdemo/static/js/2.a23894df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GestIA-Webdemo/static/js/2.a23894df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746a62aee4b5a5acb68c",
+    "url": "/GestIA-Webdemo/static/js/main.599f6ab1.chunk.js"
+  },
+  {
+    "revision": "ca6c01e042739340430d",
+    "url": "/GestIA-Webdemo/static/js/runtime-main.5334eb19.js"
+  }
+]);
