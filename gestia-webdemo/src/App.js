5d3aa259a9e8e9bcaa48ff0810567f82f2d15513
Arturacu <arturoacuaviva@gmail.com>
2020-09-13 19:38:03 +0200

    Added sprite component and wizard assets

--- gestia-webdemo/src/App.js
+++ gestia-webdemo/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Sprite from "./game/components/sprite"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="zone-container">
+            <Sprite image={process.env.PUBLIC_URL + "/sprites/wizard/Idle.png"} data= {{ x: 0, y:0, w:231, h:190, }} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
