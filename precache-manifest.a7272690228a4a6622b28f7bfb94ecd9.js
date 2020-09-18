4ca550aa95059d052d5c04dde726213c2569182e
Arturacu <arturacu@ucm.es>
2020-09-18 20:33:18 +0200

    Add files via upload

---
+++ precache-manifest.a7272690228a4a6622b28f7bfb94ecd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846438671af5204a7db89bb1157bb000",
+    "url": "/GestIA-Webdemo/index.html"
+  },
+  {
+    "revision": "50cc15b5602e5963108b",
+    "url": "/GestIA-Webdemo/static/css/main.0e9b6367.chunk.css"
+  },
+  {
+    "revision": "e0b0bac194a10e72c18e",
+    "url": "/GestIA-Webdemo/static/js/2.bd5b7a9c.chunk.js"
+  },
+  {
+    "revision": "881b4cd60baf4ebc476fd3c43b040b40",
+    "url": "/GestIA-Webdemo/static/js/2.bd5b7a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cc15b5602e5963108b",
+    "url": "/GestIA-Webdemo/static/js/main.c1976915.chunk.js"
+  },
+  {
+    "revision": "ca6c01e042739340430d",
+    "url": "/GestIA-Webdemo/static/js/runtime-main.5334eb19.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GestIA-Webdemo/precache-manifest.51f3f4dc5d95ee8e6575bdb1234d3a69.js"
+  "/GestIA-Webdemo/precache-manifest.a7272690228a4a6622b28f7bfb94ecd9.js"
 );
 
 self.addEventListener('message', (event) => {
